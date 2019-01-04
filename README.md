605b1fccd1c2f9e8c4e30b86e4a9a668b269ebd2
Matheos Mattsson <matheos.mattsson@gmail.com>
2019-01-04 22:11:28 +0200

    Added word stats. Added setup section in code. Added search for word function.

--- README.md
+++ README.md
@@ -4,4 +4,6 @@ By Matheos Mattsson
 ## Requirements
 - **Python 3**
 
+Setup: *Change 'margin of error', min word length for popular words and number of popular words to be listed in parser.py Setup section*
+
 Usage: *python parser.py 'path/to/whatsapp_log_file.txt'*
--- parser.py
+++ parser.py
@@ -1,16 +1,32 @@
 import sys
 import re
 
+# -----------------------------------------SETUP-----------------------------------------
+
 # "Margin of error" --- This is the minimum amount of messages a "user" has to have to be considered a user.
 # This is required since the regex below might on special occasions match things in the chat messages too...
-MIN_MSGS = 5
+MIN_MSGS = 4
 MIN_SYMBOLS = 50
+
+# The amount of popular words to be listed (from most used to least)
+NO_POPULAR_WORDS = 10
+
+# Minimum letters per word in popular words
+MIN_POP_LENGTH = 0
+
+# ---------------------------------------------------------------------------------------
 
 persons_msgs = {}
 
 persons_symbols = {}
 
+words = {}
+
+words_searchable = {}
+
 username_regex = r'-\s[^:]*[a-zA-Z]+[^:]*:'
+
+msg_regex = r'-\s[^:]*[a-zA-Z]+[^:]*:.*'
 
 start_date = ""
 
@@ -22,8 +38,22 @@ def parse_lines():
     global stop_date
     global persons_msgs
     global persons_symbols
+    global words
     i = 0
     for line in file.readlines():
+
+        message = re.search(msg_regex, line)  # Find username+message
+
+        if message is not None:
+            message = message.group(0).split(":", 1)[-1]  # Chop away username
+        else:
+            message = line  # If no username on line, just take whole thing
+
+        for word in message.split():
+            word = word.lower()
+            if (len(word) >= MIN_POP_LENGTH) and (word[0] != "<") and (word[len(word)-1] != ">"):  # No whatsapp tags
+                words[word] = words.get(word, 0) + 1
+
         if i == 0:
             start_date = line[0:11]
         else:
@@ -43,6 +73,10 @@ def output_info():
     persons_msgs = [(k, persons_msgs[k]) for k in sorted(persons_msgs, key=persons_msgs.get, reverse=True)]
     global persons_symbols
     persons_symbols = [(k, persons_symbols[k]) for k in sorted(persons_symbols, key=persons_symbols.get, reverse=True)]
+    global words
+    global words_searchable
+    words_searchable = words.copy()
+    words = [(k, words[k]) for k in sorted(words, key=words.get, reverse=True)]
     summa = 0
     for k, v in persons_msgs:
         if v > MIN_MSGS:
@@ -67,7 +101,36 @@ def output_info():
         if v > MIN_SYMBOLS:
             print(k+":"+str(v)+" ("+str(round((v/float(sum_symbols))*100))+"%)")
     print("Total symbols: "+str(sum_symbols)+"\n")
+
+    print("---Most Popular words - Minimum word length = " + str(MIN_POP_LENGTH) + "---")
+    i = 0
+    for k, v, in words:
+        if i == NO_POPULAR_WORDS:
+            break
+        i += 1
+        print(str(i)+". "+k + ":" + str(v))
             
+
+def user_option():
+    choice = input("Choose an option\n")
+    if choice == "1":
+        find_word()
+        return True
+    elif choice == "0" or choice == "q":
+        return False
+    return True
+
+
+def find_word():
+    while True:
+        word = input("Enter a word (type q to quit): ")
+        if word == "q":
+            break
+        result = words_searchable.get(word.lower())
+        if result is None:
+            result = "0"
+        print(word+" occurs a number of "+str(result)+" times!\n")
+
 
 print("\n------------ Welcome to WhatsApp Log Parser by Matheos Mattsson ------------\n")
 try:
@@ -78,6 +141,11 @@ try:
 
     output_info()
     file.close()
+
+    print("\nWhat do you want to do?\nq = quit\n1 = Search for a word \n")
+    while True:
+        if not user_option():
+            break
 
 except Exception as e:
     print("No input file specified! Give the file as an argument: python parser.py 'path_to_file'")
